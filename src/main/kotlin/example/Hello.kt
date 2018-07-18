39329f490bcfbeed722a9b06d72aabd0f182a492
freewind <nowindlee@gmail.com>
2018-07-18 23:54:18 +0800

    remove unnecessary code

--- src/main/kotlin/example/Hello.kt
+++ src/main/kotlin/example/Hello.kt
@@ -2,15 +2,12 @@ package example
 
 import chrome.tabs.QueryInfo
 import kotlinjs.common.JsObj
-import kotlin.browser.document
 import kotlin.browser.window
 
 fun main(args: Array<String>) {
-    document.addEventListener("DOMContentLoaded", {
-        getCurrentTabUrl { url ->
-            window.alert("current url is: $url")
-        }
-    })
+    getCurrentTabUrl { url ->
+        window.alert("current url is: $url")
+    }
 }
 
 private fun getCurrentTabUrl(callback: (String) -> Unit) {
